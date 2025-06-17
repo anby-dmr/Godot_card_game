c959ba4ee37e72397f43a21f1dfa41a81eb3ae31
anby <13177850569@163.com>
2025-06-17 11:30:06 +0800

    Lec2 bug fix about hovering.

--- scripts/card_manager.gd
+++ scripts/card_manager.gd
@@ -4,10 +4,12 @@ var COLLISION_MASK_CARD = 1
 
 var card_being_dragged
 var screen_size
+var is_hovering_on_card
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	is_hovering_on_card = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -33,10 +35,26 @@ func connect_card_signals(card):
 	card.connect("hovered_off", on_hovered_off_card)
 
 func on_hovered_card(card):
-	highlight_card(card, true)
+	if !is_hovering_on_card:
+		highlight_card(card, true)
+		is_hovering_on_card = true
 
 func on_hovered_off_card(card):
+	# This is not correct.
+	# This slot func will still be triggered when move from one card to another
+	# But before is_hovering_on_card = false, 
+	# the on_hovered_card will be called first (signal emits only once here),
+	# which cause bug.
+	#if is_hovering_on_card:
+		#highlight_card(card, false)
+		#is_hovering_on_card = false
+		
 	highlight_card(card, false)
+	var new_card_hovered = raycast_check_for_card()
+	if new_card_hovered:
+		highlight_card(new_card_hovered, true)
+	else:
+		is_hovering_on_card = false
 
 func highlight_card(card, hovered):
 	if hovered:
@@ -46,7 +64,8 @@ func highlight_card(card, hovered):
 		card.scale = Vector2(1, 1)
 		card.z_index = 1
 
-# Get the card object clicked by mouse
+# Get the card object hovered by mouse.
+# Clicked check is in _input() method.
 func raycast_check_for_card():
 	var state_space = get_world_2d().direct_space_state
 	var clickParams = PhysicsPointQueryParameters2D.new()
